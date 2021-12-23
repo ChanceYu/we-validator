63dd75a64408bbeb2d97ede89c7b67a5987a5a70
watsonhaw <348748267@qq.com>
2021-12-23 11:57:19 +0800

    fix: #21

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,25 +1,35 @@
-declare module "we-validator" {
-    type Rule = Function | RegExp;
-    type RuleOption = { rule: Rule; message: string };
-    type Options = { 
-      rules: object,
-      messages: object,
-      onMessage?: Function,
-      multiple?: boolean
-    }
-    class WeValidator {
-      constructor(options: Options);
-      static RULES: object;
-      static addRule(ruleName: string, ruleOption: RuleOption): void;
-      static checkValue(ruleName: string, value: string, param: any, skip: boolean): boolean;
-      options: Options;
-      required: Function;
-      checkData(data: object, onMessage?: Function, showMessage?: boolean, fieldMap?: object): boolean;
-      checkFields(data: object, onMessage?: Function, showMessage?: boolean): boolean;
-      isValid(data: object, fields: Array<string>): boolean;
-      addRules(options: Options): void;
-      removeRules(fields: Array<string>): void;
-    }
-    export = WeValidator
+declare module 'we-validator' {
+  type Rule = Function | RegExp;
+  type RuleOption = { rule: Rule; message: string };
+  type Options = {
+    rules: object;
+    messages: object;
+    onMessage?: Function;
+    multiple?: boolean;
+  };
+
+  class WeValidator {
+    constructor(options: Options);
+
+    static RULES: object;
+
+    static addRule(ruleName: string, ruleOption: RuleOption): void;
+
+    static checkValue(ruleName: string, value: string, param: any, skip: boolean): boolean;
+
+    options: Options;
+    required: Function;
+
+    checkData(data: object, onMessage?: Function, showMessage?: boolean, fieldMap?: object): boolean;
+
+    checkFields(data: object, fields: [], onMessage?: Function, showMessage?: boolean): boolean;
+
+    isValid(data: object, fields: Array<string>): boolean;
+
+    addRules(options: Options): void;
+
+    removeRules(fields: Array<string>): void;
   }
-  
+
+  export = WeValidator;
+}
